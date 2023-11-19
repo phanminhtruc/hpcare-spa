11de380a6b2b3bda849c2d47659f4071f9b2034a
PMT <pmtruc2012@gmail.com>
2023-11-19 16:45:39 +0700

    first commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,30 @@
+export default function Footer() {
+  return (
+    <footer className='bg-neutral-100 py-16'>
+      <div className='container'>
+        <div className='grid grid-cols-1 gap-4 lg:grid-cols-3'>
+          <div className='lg:col-span-1'>
+            <div>© 2022 HP Care. Tất cả các quyền được bảo lưu.</div>
+          </div>
+          <div className='lg:col-span-2'>
+            <div>
+              
+            </div>
+          </div>
+        </div>
+        <div className='mt-10 text-center text-sm'>
+          <div>HP Care</div>
+          <div className='mt-6'>
+            Địa chỉ: 188/16 Khu phố 9, phường Tam Hiệp, thành phố Biên Hòa, tỉnh Đồng Nai. Email: hpcare1110@gmail.com
+          </div>
+          <div className='mt-2'>
+            Chịu Trách Nhiệm Quản Lý Nội Dung: Hoàng Thị Phương - Điện thoại liên hệ: 0972232013
+          </div>
+          <div className='mt-2'>
+          </div>
+          <div className='mt-2'>© 2015 - Bản quyền thuộc HP Care</div>
+        </div>
+      </div>
+    </footer>
+  )
+}
